5250a296dfff940af8efd0739c5d6668289e380e
Панов Даниил <kartoshkavkusnay@gmail.com>
2025-03-13 14:23:58 +0200

    add

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -8,6 +8,6 @@ export const PLUGIN_VERSION = {
     "suffix": "7bf7ea0-dirty",
     "semverString": null,
     "name": "movie-catalog",
-    "version": "1.1.7",
+    "version": "1.1.8",
     "main": "main.js"
 };
